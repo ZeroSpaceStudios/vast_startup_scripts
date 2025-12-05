036a9ba45150bee39ebae11dc527303bce2e168d
derekhsiaochan <derekhsiaochan@gmail.com>
2025-12-05 14:15:18 -0500

    added more custom nodes

--- vast_v1.sh
+++ vast_v1.sh
@@ -24,6 +24,15 @@ CUSTOM_NODES=(
     "https://github.com/ltdrdata/ComfyUI-Manager"
     "https://github.com/rgthree/rgthree-comfy.git"
     "https://github.com/kijai/ComfyUI-KJNodes.git"
+    "https://github.com/ShmuelRonen/ComfyUI-VideoUpscale_WithModel.git"
+    "https://github.com/neonvoid/ComfyUI-GIMM-VFI_nvFork.git"
+    "https://github.com/neonvoid/ComfyUI-SAM3_nvFork.git"
+    "https://github.com/kijai/ComfyUI-WanVideoWrapper.git"
+    "https://github.com/neonvoid/NV_Comfy_Utils.git"
+    
+
+
+
     # Add more URLs below:
 )
 
